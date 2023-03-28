00c2030b2b670e2af33b277123ccb8aecbfc4888
santhushakodi <santhushakodi123@gmail.com>
2023-03-28 16:34:43 +0530

    intergrate with mysql

--- murmur-detection.py
+++ murmur-detection.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask, jsonify, request
 import pickle
 from flask import Flask, render_template
-
+import mysql.connector
 
 
 
@@ -33,14 +33,46 @@ def result():
         print()
         return jsonify(error="Please try again.")
     
-    
-
 
     file = request.files.get('feature')
     feature  = np.load(file)
     result = model.predict(feature)
     result = result.tolist()
     return jsonify(prediction=result)
+
+@app.route('/writedb', methods=['POST'])
+def update():
+    mydb = mysql.connector.connect(
+        host="demo-database-1.cvs5fl0cptbn.eu-north-1.rds.amazonaws.com",
+        user="admin",
+        password="admin123",
+        database="demodb"
+        )
+    
+    data = request.get_json()
+    id = data.get("id")
+    murmur = data.get("murmur")
+    print(id,murmur)
+    mycursor = mydb.cursor()
+
+    
+    query1 = "INSERT INTO patients (id, murmur) VALUES (%s, %s)"
+    data = (id, murmur)
+    mycursor.execute(query1, data)
+    # commit the transaction
+    mydb.commit()
+
+    query2 = "SELECT * FROM patients WHERE id=%s"
+    data2 = (id,)
+    mycursor.execute(query2, data2)
+    # fetch the result
+    result = mycursor.fetchone()
+    mycursor.close()
+    mydb.close()
+
+    resp = {"id":result[0],"mumur":result[1]}
+    return jsonify(resp)
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
